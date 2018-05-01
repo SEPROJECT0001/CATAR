86131876fb4f4c660c9e71341755675392743b4e
Hiba-Hansali <36371232+Hiba-Hansali@users.noreply.github.com>
2018-05-01 10:17:35 +0100

    Update tri.sh
    tri par insertion

--- tri.sh
+++ tri.sh
@@ -60,7 +60,34 @@ echo "Enter the number :"
             do
                echo ${arr[$i]}
             done
-
+            
+            
+##### tri par insertion ####
+echo "Enter the number :"
+             read n
+             echo "Enter a number in an array :"
+             for (( i=0;i<n;i++ ))
+             do
+                read arr[$i]
+             done
+   
+             for (( i=1;i<n;i++ ))
+             do
+                j=$i-1
+                temp=${arr[$i]}
+                while (( j>=0 && arr[j]>temp ))
+                do
+                   arr[$j+1]=${arr[$j]}
+                   j=$j-1
+                done
+                arr[j+1]=$temp 
+             done
+             echo "Printing sorted array :"
+             for (( i=0;i<n;i++ ))
+             do
+                echo ${arr[$i]}
+             done
+             
 
 
 
