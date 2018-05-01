8ddd8e54d44bde4c89317fcb86879bbc27b1fd95
ELHASSANE <hassanlhyani7@gmail.com>
2018-05-01 10:10:37 +0100

    tri par selection

--- tri.sh
+++ tri.sh
@@ -28,8 +28,38 @@ read -p "entrer nombre de lignes : " SIZE
              done
              
 
+##### tri par selection ####
 
-
+echo "Enter the number :"      
+             read n
+             echo "Enter number in an array :"
+             for (( i=0;i<n;i++ ))
+             do
+                read arr[$i]
+             done
+             
+             for (( i=0;i<n-1;i++ ))
+             do
+                small=${arr[$i]}
+                index=$i
+                for (( j=i+1;j<n;j++ ))
+                do
+                   if (( arr[j]<small ))
+                   then
+                         small=${arr[$j]}
+                         index=$j
+                   fi
+               done
+               temp=${arr[$i]}
+               arr[$i]=${arr[$index]}
+               arr[$index]=$temp   
+            done
+ 
+            echo "Printing sorted array :"
+            for (( i=0;i<n;i++ ))   
+            do
+               echo ${arr[$i]}
+            done
 
 
 
